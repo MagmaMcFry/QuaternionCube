1d9d5e6b9d5178eb82751dc3bb0e2508768b415c
MagmaMcFry <magmamcfry@gmail.com>
2021-02-05 19:47:30 +0100

    Working prototype

--- main.js
+++ main.js
@@ -1,39 +1,117 @@
 'use strict';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10);
-camera.position.z = 3;
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+window.addEventListener("load", function() {
+	const canvas = document.getElementById("canvas");
+	const scene = new THREE.Scene();
+	const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 10);
+	camera.position.z = 3;
+	const renderer = new THREE.WebGLRenderer();
+	renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+	canvas.appendChild(renderer.domElement);
 
-// const geometry = new THREE.BoxGeometry();
-// const material = new THREE.MeshBasicMaterial({color: 0xff0000});
-// const cubemesh = new THREE.Mesh(geometry, material);
-// scene.add(cubemesh);
+	// const geometry = new THREE.BoxGeometry();
+	// const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+	// const cubemesh = new THREE.Mesh(geometry, material);
+	// scene.add(cubemesh);
 
-const cube = rubikscube(3);
-const cubemodel = cube.R_get_root();
-scene.add(cubemodel);
+	var cube = rubikscube(2);
+	var cubemodel = cube.get_root();
+	scene.add(cubemodel);
 
-var current_amount = 0;
-var current_rotation = 0;
-function update_cube() {
-	current_amount += 0.02;
-	if (current_amount >= 1) {
-		cube.apply_move(current_rotation);
-		cube.R_update_plates();
-		current_amount = 0;
-		current_rotation = Math.floor(Math.random() * cube.get_move_count());
-	}
-	cube.R_set_current_rotation(current_rotation, current_amount);
-}
+	let current_amount = 0;
+	let current_move = 0;
 
-function animate() {
+	function apply_move(move) {
+		current_amount = -1;
+			current_move = move;
+			cube.apply_move(current_move);
+	};
+
+	function update_cube() {
+		current_amount += 0.1;
+		if (current_amount > 0) {
+			current_amount = 0;
+		}
+
+	//	if (current_amount == 0) {
+	//		apply_move(Math.floor(Math.random() * cube.get_move_count()));
+	//	}
+
+		cube.set_current_rotation(current_move, current_amount);
+	};
+
+	let mouse_down = false;
+	let moused_panel = -1;
+	let clicked_panel = -1;
+	let mouse = new THREE.Vector2();
+	let raycaster = new THREE.Raycaster();
+
+	function getMousedPanel() {
+		raycaster.setFromCamera(mouse, camera);
+		return cube.get_panel(raycaster);
+	};
+
+
+	renderer.domElement.addEventListener("mousemove", function(event) {
+		event.preventDefault();
+		let oldmouse = mouse.clone();
+		mouse.x = ( event.clientX / canvas.clientWidth ) * 2 - 1;
+		mouse.y = 1 - 2 * ( event.clientY / canvas.clientHeight );
+
+		if (mouse_down && clicked_panel == -1) {
+			let dmouse = mouse.clone().sub(oldmouse);
+			let target = new THREE.Quaternion(-100*dmouse.y, 100*dmouse.x, 0, 1).normalize();
+			let rotation = new THREE.Quaternion();
+			rotation.rotateTowards(target, 2*dmouse.length());
+			cubemodel.quaternion.multiplyQuaternions(rotation, cubemodel.quaternion);
+		}
+	});
+
+	renderer.domElement.addEventListener("resize", function() {
+		camera.aspect = canvas.clientWidth / canvas.clientHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize( canvas.clientWidth, canvas.clientHeight );
+	});
+
+	renderer.domElement.addEventListener("mousedown", function() {
+		mouse_down = true;
+		clicked_panel = getMousedPanel();
+		//console.log("Clicked panel", clicked_panel);
+	});
+
+	renderer.domElement.addEventListener("mouseup", function() {
+		if (clicked_panel != -1 && moused_panel != -1) {
+			let move = cube.try_rotate(clicked_panel, moused_panel);
+			if (move != -1) {
+				apply_move(move);
+			}
+		}
+		mouse_down = false;
+		clicked_panel = -1;
+	});
+
+	document.addEventListener("keypress", function(e) {
+		console.log("Code: " + e.code);
+		if (e.code === "NumpadSubtract") {
+
+		}
+	});
+
+	//renderer.domElement.addEventListener("click", function() {
+	//	apply_move(Math.floor(Math.random() * cube.get_move_count()));
+	//});
+
+	function animate() {
+		requestAnimationFrame(animate);
+		//let target = new THREE.Quaternion(0.5, 0.5, 0.5, 0.5);
+		//let rotation = new THREE.Quaternion().rotateTowards(target, 0.01);
+		//cubemodel.quaternion.multiply(rotation);
+		update_cube();
+		moused_panel = getMousedPanel();
+		cube.update_panels(moused_panel, clicked_panel);
+		renderer.render(scene, camera);
+	};
+
 	requestAnimationFrame(animate);
-	cubemodel.rotation.x += 0.01;
-	cubemodel.rotation.z += 0.01;
-	update_cube();
-	renderer.render(scene, camera);
-}
-requestAnimationFrame(animate);
+
+});
--- rubikscube.js
+++ rubikscube.js
@@ -9,7 +9,8 @@ function rubikscube(n) {
 	// Red, orange, blue, green, yellow, white
 	// R, L, B, F, D, U
 	const black_color = 0x808080;
-	const side_colors = [0xff0000, 0xff8000, 0x0000ff, 0x00ff00, 0xffff00, 0xffffff];
+	const side_colors = [0xbb0000, 0xbb6000, 0x0000bb, 0x00bb00, 0xbbbb00, 0xbbbbbb];
+	const hilit_side_colors = [0xff0000, 0xff8000, 0x0000ff, 0x00ff00, 0xffff00, 0xffffff];
 
 	const directions = [
 		[-1, 0, 0],
@@ -67,212 +68,255 @@ function rubikscube(n) {
 		],
 	];
 
-	let plate_count = undefined;
-	let plate_positions = [];
-	let plate_colors = [];
+	let panel_count = undefined;
+	let panel_positions = [];
+	let panel_colors = [];
 	let move_count = undefined;
 	let move_permutations = [];
 	let move_affected_cubies = [];
 	let rot_directions = [];
 
 
+
+	const init = function() {
+		// panels
+		for (let z = 0; z < n; ++z) {
+			for (let y = 0; y < n; ++y) {
+				for (let x = 0; x < n; ++x) {
+					const outsides = [x == 0, x == n-1, y == 0, y == n-1, z == 0, z == n-1];
+					for (let s = 0; s < 6; ++s) {
+						if (outsides[s]) {
+							panel_positions.push({vec: [x, y, z], x: x, y: y, z: z, side: s});
+							panel_colors.push(s);
+						}
+					}
+				}
+			}
+		}
+		panel_count = panel_colors.length;
+
+		// Moves
+		for (let slice = 0; slice < n; ++slice) {
+			for (let side = 0; side < 6; ++side) {
+				let move = [];
+				for (let i = 0; i < panel_count; ++i) {
+					move.push(rotate_panel_slow(i, side, slice));
+				}
+				move_permutations.push(move);
+				rot_directions.push(directions[side]);
+				const current_move_affected_cubies = [];
+				move_affected_cubies.push(current_move_affected_cubies);
+				for (let z = 0; z < n; ++z) {
+					current_move_affected_cubies.push([]);
+					for (let y = 0; y < n; ++y) {
+						current_move_affected_cubies[z].push([]);
+						for (let x = 0; x < n; ++x) {
+							current_move_affected_cubies[z][y].push(is_cubie_affected_slow([x, y, z], side, slice));
+						}
+					}
+				}
+			}
+		}
+		move_count = 6 * n;
+	};
+
+	const find_panel = function(x, y, z, s) {
+		for (let i = 0; i < panel_count; ++i) {
+			const panel = panel_positions[i];
+			if (panel.x == x && panel.y == y && panel.z == z && panel.side == s) {
+				return i;
+			}
+		}
+		console.error("Cannot find given panel!");
+		return -1;
+	};
+
+	const apply_rotation = function(vector, rot_side) {
+		const mat = rotation_mats[rot_side];
+		return [
+			mat[0][0] * vector[0] + mat[0][1] * vector[1] + mat[0][2] * vector[2],
+			mat[1][0] * vector[0] + mat[1][1] * vector[1] + mat[1][2] * vector[2],
+			mat[2][0] * vector[0] + mat[2][1] * vector[1] + mat[2][2] * vector[2],
+		];
+	};
+
+	const rotate_cubie = function(cubie_pos, rot_side) {
+		let vec = apply_rotation(cubie_pos, rot_side);
+		const compensation = apply_rotation([1,1,1], rot_side);
+		for (let i = 0; i < 3; ++i) {
+			if (compensation[i] < 0) {
+				vec[i] += n - 1;
+			}
+		}
+		return vec;
+	};
+
+	const rotate_side = function(affected_side, rot_side) {
+		return rotations[rot_side][affected_side];
+	};
+
+	const is_cubie_affected_slow = function(cubie_pos, rot_side, rot_slice) {
+		return cubie_pos[rotation_dims[rot_side]] == rot_slice;
+	};
+
+	const rotate_panel_slow = function(panel_id, rot_side, rot_slice) {
+		const pos = panel_positions[panel_id];
+		const oldvec = pos.vec;
+		const oldside = pos.side;
+		if (!is_cubie_affected_slow(pos.vec, rot_side, rot_slice)) {
+			return panel_id;
+		}
+		const newvec = rotate_cubie(oldvec, rot_side);
+		const newside = rotate_side(oldside, rot_side);
+		return find_panel(newvec[0], newvec[1], newvec[2], newside);
+	};
+
+	const is_cubie_affected = function(cubie_x, cubie_y, cubie_z, move_id) {
+		return move_affected_cubies[move_id][cubie_z][cubie_y][cubie_x];
+	};
+
+	const apply_move = function(move_id) {
+		const move = move_permutations[move_id];
+		const old_panel_colors = panel_colors.slice();
+		for (let i = 0; i < panel_count; ++i) {
+			panel_colors[move[i]] = old_panel_colors[i];
+		}
+	};
+
+	const try_rotate = function(old_panel, new_panel) {
+		if (old_panel == new_panel) return -1;
+		let old_pos = panel_positions[old_panel];
+		let new_pos = panel_positions[new_panel];
+
+		let best_move = -1;
+		let best_distance = (n+2)*(n+2);
+		for (let m = 0; m < move_count; ++m) {
+			let moved_panel = move_permutations[m][old_panel];
+			let moved_pos = panel_positions[moved_panel];
+			if (moved_panel != old_panel && moved_pos.side != old_pos.side) {
+				let distance = Math.abs(moved_pos.x - new_pos.x)
+					+ Math.abs(moved_pos.y - new_pos.y)
+					+ Math.abs(moved_pos.z - new_pos.z)
+					+ (n+2) * (moved_pos.side != new_pos.side);
+				if (distance < best_distance) {
+					best_move = m;
+					best_distance = distance;
+				}
+			}
+		}
+		return best_move;
+	};
+
+
 	let R_cubies = [];
-	let R_plates = [];
+	let R_panels = [];
 	let R_color_materials = [];
+	let R_hilit_color_materials = [];
 	let R_root = undefined;
 	let R_main = undefined;
 
-
-	const Cube = {
-		init: function() {
-			// Plates
-			for (var z = 0; z < n; ++z) {
-				for (var y = 0; y < n; ++y) {
-					for (var x = 0; x < n; ++x) {
-						const outsides = [x == 0, x == n-1, y == 0, y == n-1, z == 0, z == n-1];
-						for (var s = 0; s < 6; ++s) {
-							if (outsides[s]) {
-								plate_positions.push({vec: [x, y, z], x: x, y: y, z: z, side: s});
-								plate_colors.push(s);
-							}
-						}
-					}
+	const R_init = function() {
+		R_root = new THREE.Object3D();
+		R_main = new THREE.Object3D();
+		const black_material = new THREE.MeshBasicMaterial({color: black_color});
+		const cubie_geometry = new THREE.BoxGeometry(1, 1, 1);
+		const panel_geometry = new THREE.BoxGeometry(0.9, 0.9, 0.9);
+		for (let c = 0; c < 6; ++c) {
+			R_color_materials.push(new THREE.MeshBasicMaterial({color: side_colors[c]}));
+			R_hilit_color_materials.push(new THREE.MeshBasicMaterial({color: hilit_side_colors[c]}));
+		}
+		for (let z = 0; z < n; ++z) {
+			R_cubies.push([]);
+			for (let y = 0; y < n; ++y) {
+				R_cubies[z].push([]);
+				for (let x = 0; x < n; ++x) {
+					const cubie_object = new THREE.Object3D();
+					//const cubie_material = new THREE.MeshBasicMaterial({color: Math.floor(Math.random()*0xffffff)}); // 0x0f0f0f
+					const cubie_mesh = new THREE.Mesh(cubie_geometry, black_material);
+					cubie_mesh.position.x = x + (1-n)/2;
+					cubie_mesh.position.y = y + (1-n)/2;
+					cubie_mesh.position.z = z + (1-n)/2;
+					cubie_object.add(cubie_mesh);
+					R_cubies[z][y].push(cubie_object);
+					R_main.add(cubie_object);
 				}
 			}
-			plate_count = plate_colors.length;
+		}
+		for (let i = 0; i < panel_count; ++i) {
+			const panel = panel_positions[i];
+			const panel_material = R_color_materials[panel_colors[i]];
+			const panel_mesh = new THREE.Mesh(panel_geometry, panel_material);
+			const panel_direction = directions[panel.side];
+			panel_mesh.position.x = panel.x + 0.1*panel_direction[0] + (1-n)/2;
+			panel_mesh.position.y = panel.y + 0.1*panel_direction[1] + (1-n)/2;
+			panel_mesh.position.z = panel.z + 0.1*panel_direction[2] + (1-n)/2;
+			R_cubies[panel.z][panel.y][panel.x].add(panel_mesh);
+			R_panels.push(panel_mesh);
+		}
+		R_main.scale.x = 1/n;
+		R_main.scale.y = 1/n;
+		R_main.scale.z = 1/n;
 
-			// Moves
-			for (var slice = 0; slice < n; ++slice) {
-				for (var side = 0; side < 6; ++side) {
-					let move = [];
-					for (var i = 0; i < plate_count; ++i) {
-						move.push(this.rotate_plate_slow(i, side, slice));
-					}
-					move_permutations.push(move);
-					rot_directions.push(directions[side]);
-					const current_move_affected_cubies = [];
-					move_affected_cubies.push(current_move_affected_cubies);
-					for (var z = 0; z < n; ++z) {
-						current_move_affected_cubies.push([]);
-						for (var y = 0; y < n; ++y) {
-							current_move_affected_cubies[z].push([]);
-							for (var x = 0; x < n; ++x) {
-								current_move_affected_cubies[z][y].push(this.is_cubie_affected_slow([x, y, z], side, slice));
-							}
-						}
-					}
-				}
-			}
-			move_count = 6 * n;
-		},
+		R_root.add(R_main);
 
-		find_plate: function(x, y, z, s) {
-			for (var i = 0; i < plate_count; ++i) {
-				const plate = plate_positions[i];
-				if (plate.x == x && plate.y == y && plate.z == z && plate.side == s) {
-					return i;
-				}
-			}
-			console.error("Cannot find given plate!");
-			return -1;
-		},
-
-		apply_rotation: function(vector, rot_side) {
-			const mat = rotation_mats[rot_side];
-			return [
-				mat[0][0] * vector[0] + mat[0][1] * vector[1] + mat[0][2] * vector[2],
-				mat[1][0] * vector[0] + mat[1][1] * vector[1] + mat[1][2] * vector[2],
-				mat[2][0] * vector[0] + mat[2][1] * vector[1] + mat[2][2] * vector[2],
-			];
-		},
-
-		rotate_cubie: function(cubie_pos, rot_side) {
-			let vec = this.apply_rotation(cubie_pos, rot_side);
-			const compensation = this.apply_rotation([1,1,1], rot_side);
-			for (var i = 0; i < 3; ++i) {
-				if (compensation[i] < 0) {
-					vec[i] += n - 1;
-				}
-			}
-			return vec;
-		},
-
-		rotate_side: function(affected_side, rot_side) {
-			return rotations[rot_side][affected_side];
-		},
-
-		is_cubie_affected_slow: function(cubie_pos, rot_side, rot_slice) {
-			return cubie_pos[rotation_dims[rot_side]] == rot_slice;
-		},
-
-		rotate_plate_slow: function(plate_id, rot_side, rot_slice) {
-			const pos = plate_positions[plate_id];
-			const oldvec = pos.vec;
-			const oldside = pos.side;
-			if (!this.is_cubie_affected_slow(pos.vec, rot_side, rot_slice)) {
-				return plate_id;
-			}
-			const newvec = this.rotate_cubie(oldvec, rot_side);
-			const newside = this.rotate_side(oldside, rot_side);
-			return this.find_plate(newvec[0], newvec[1], newvec[2], newside);
-		},
-
-		is_cubie_affected: function(cubie_x, cubie_y, cubie_z, rot_id) {
-			return move_affected_cubies[rot_id][cubie_z][cubie_y][cubie_x];
-		},
-
-		rotate_plate: function(plate_id, rot_id) {
-			return move_permutations[rot_id][plate_id];
-		},
-
-		get_move_count: function() {
-			return move_count;
-		},
-
-		apply_move: function(rot_id) {
-			const move = move_permutations[rot_id];
-			const old_plate_colors = plate_colors.slice();
-			for (var i = 0; i < plate_count; ++i) {
-				plate_colors[move[i]] = old_plate_colors[i];
-			}
-		},
-
-		R_init: function() {
-			R_root = new THREE.Object3D();
-			R_main = new THREE.Object3D();
-			const black_material = new THREE.MeshBasicMaterial({color: black_color});
-			const cubie_geometry = new THREE.BoxGeometry(1, 1, 1);
-			const plate_geometry = new THREE.BoxGeometry(0.9, 0.9, 0.9);
-			for (var c = 0; c < 6; ++c) {
-				R_color_materials.push(new THREE.MeshBasicMaterial({color: side_colors[c]}));
-			}
-			for (var z = 0; z < n; ++z) {
-				R_cubies.push([]);
-				for (var y = 0; y < n; ++y) {
-					R_cubies[z].push([]);
-					for (var x = 0; x < n; ++x) {
-						const cubie_object = new THREE.Object3D();
-						//const cubie_material = new THREE.MeshBasicMaterial({color: Math.floor(Math.random()*0xffffff)}); // 0x0f0f0f
-						const cubie_mesh = new THREE.Mesh(cubie_geometry, black_material);
-						cubie_mesh.position.x = x + (1-n)/2;
-						cubie_mesh.position.y = y + (1-n)/2;
-						cubie_mesh.position.z = z + (1-n)/2;
-						cubie_object.add(cubie_mesh);
-						R_cubies[z][y].push(cubie_object);
-						R_main.add(cubie_object);
-					}
-				}
-			}
-			for (var i = 0; i < plate_count; ++i) {
-				const plate = plate_positions[i];
-				const plate_material = R_color_materials[plate_colors[i]];
-				const plate_mesh = new THREE.Mesh(plate_geometry, plate_material);
-				const plate_direction = directions[plate.side];
-				plate_mesh.position.x = plate.x + 0.1*plate_direction[0] + (1-n)/2;
-				plate_mesh.position.y = plate.y + 0.1*plate_direction[1] + (1-n)/2;
-				plate_mesh.position.z = plate.z + 0.1*plate_direction[2] + (1-n)/2;
-				R_cubies[plate.z][plate.y][plate.x].add(plate_mesh);
-				R_plates.push(plate_mesh);
-			}
-			R_main.scale.x = 1/n;
-			R_main.scale.y = 1/n;
-			R_main.scale.z = 1/n;
-
-			R_root.add(R_main);
-		},
-
-		R_update_plates: function() {
-			for (var i = 0; i < plate_count; ++i) {
-				R_plates[i].material = R_color_materials[plate_colors[i]];
-			}
-		},
-
-		R_set_current_rotation: function(rot_id, amount) {
-			const rot_dir = rot_directions[rot_id];
-			for (var z = 0; z < n; ++z) {
-				for (var y = 0; y < n; ++y) {
-					for (var x = 0; x < n; ++x) {
-						const cubie = R_cubies[z][y][x];
-						if (this.is_cubie_affected(x, y, z, rot_id)) {
-							cubie.rotation.x = amount * rot_dir[0] * Math.PI/2;
-							cubie.rotation.y = amount * rot_dir[1] * Math.PI/2;
-							cubie.rotation.z = amount * rot_dir[2] * Math.PI/2;
-						} else {
-							cubie.rotation.x = 0;
-							cubie.rotation.y = 0;
-							cubie.rotation.z = 0;
-						}
-					}
-				}
-			}
-		},
-
-		R_get_root: function() {
-			return R_root
-		},
 	};
 
-	Cube.init();
-	Cube.R_init();
-	return Cube;
+	const R_update_panels = function(...highlighted_panels) {
+		for (let i = 0; i < panel_count; ++i) {
+			if (highlighted_panels.includes(i)) {
+				R_panels[i].material = R_hilit_color_materials[panel_colors[i]];
+			} else {
+				R_panels[i].material = R_color_materials[panel_colors[i]];
+			}
+		}
+	};
+
+	const R_set_current_rotation = function(rot_id, amount) {
+		const rot_dir = rot_directions[rot_id];
+		for (let z = 0; z < n; ++z) {
+			for (let y = 0; y < n; ++y) {
+				for (let x = 0; x < n; ++x) {
+					const cubie = R_cubies[z][y][x];
+					if (is_cubie_affected(x, y, z, rot_id)) {
+						cubie.rotation.x = amount * rot_dir[0] * Math.PI/2;
+						cubie.rotation.y = amount * rot_dir[1] * Math.PI/2;
+						cubie.rotation.z = amount * rot_dir[2] * Math.PI/2;
+					} else {
+						cubie.rotation.x = 0;
+						cubie.rotation.y = 0;
+						cubie.rotation.z = 0;
+					}
+				}
+			}
+		}
+	};
+
+	const R_get_panel = function(raycaster) {
+		let result_array = raycaster.intersectObject(R_root, true);
+		if (result_array.length == 0) {
+			//console.log("No intersections found");
+			return -1;
+		}
+		for (let i = 0; i < R_panels.length; ++i) {
+			if (R_panels[i] == result_array[0].object) {
+				return i;
+			}
+		}
+		//console.log("Invalid intersection found");
+		return -1;
+	};
+
+	init();
+	R_init();
+
+
+	return {
+		get_move_count: function() { return move_count; },
+		get_root: function() { return R_root; },
+		apply_move: apply_move,
+		update_panels: R_update_panels,
+		set_current_rotation: R_set_current_rotation,
+		get_panel: R_get_panel,
+		try_rotate: try_rotate,
+	};
 }
